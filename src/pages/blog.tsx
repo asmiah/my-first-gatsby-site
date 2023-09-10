c246b63f339193ce47fbeb6ada2b39677a05f98b
عبد الصمد <abdush.miah@miahtech.co.uk>
2023-09-10 10:27:55 +0100

    Finished gatsby tut Part 5

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -10,27 +10,32 @@ export interface IBlogPageProps {
 const BlogPage = (props: IBlogPageProps) => {
     return (
         <Layout pageTitle='My Blog Posts'>
-            <ul>
-                {
-                    props.data.allFile.nodes.map((node: any) => (
-                        <li key={node.name}>
-                            {node.name}
-                        </li>
-                    ))
-                }
-            </ul>
+            {
+                props.data.allMdx.nodes.map((node: any) => (
+                <article key={node.id}>
+                    <h2>{node.frontmatter.title}</h2>
+                    <p>Posted: {node.frontmatter.date}</p>
+                    <p>{node.excerpt}</p>
+                </article>
+                ))
+            }
         </Layout>
     )
 }
 
-export const query = graphql `
-    query{
-        allFile {
-            nodes {
-                name
-            }
-            }
+export const query = graphql`
+  query {
+    allMdx(sort: { frontmatter: { date: DESC }}) {
+      nodes {
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        excerpt
+      }
     }
+  }
 `
 
 export const Head = () => <Seo title="My Blog Posts" />
