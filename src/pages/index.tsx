9a65deacf2e3a8ac86b2a5a0e8ac17939b9edbdc
عبد الصمد <abdush.miah@miahtech.co.uk>
2023-09-10 05:04:29 +0100

    Finished gatsby tut Part 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
 import * as React from "react"
 import { Link, type HeadFC, type PageProps } from "gatsby"
 import Layout from "../components/layout"
+import { StaticImage } from "gatsby-plugin-image"
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <Layout pageTitle="HomePage"> 
       <p>I'm making this following the gatsby tutorial</p>
+      <StaticImage
+        alt="fast car"
+        src="../images/transporter.png"
+      />
     </Layout>
   )
 }
