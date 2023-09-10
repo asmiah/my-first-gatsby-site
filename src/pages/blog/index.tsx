6f6ffa1222949a11b01657d3a5af5fcc8d6aa055
عبد الصمد <abdush.miah@miahtech.co.uk>
2023-09-10 19:09:48 +0100

    Finished gatsby tut Part 6

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
-import Layout from '../components/layout'
-import Seo from '../components/seo'
-import { graphql } from 'gatsby'
+import Layout from '../../components/layout'
+import Seo from '../../components/seo'
+import { graphql, Link } from 'gatsby'
 
 export interface IBlogPageProps {
     data: any;
@@ -13,9 +13,11 @@ const BlogPage = (props: IBlogPageProps) => {
             {
                 props.data.allMdx.nodes.map((node: any) => (
                 <article key={node.id}>
-                    <h2>{node.frontmatter.title}</h2>
-                    <p>Posted: {node.frontmatter.date}</p>
-                    <p>{node.excerpt}</p>
+                    <h2>
+                      <Link to={`/blog/${node.frontmatter.slug}`}>
+                          {node.frontmatter.title}
+                      </Link>
+                    </h2>
                 </article>
                 ))
             }
@@ -30,6 +32,7 @@ export const query = graphql`
         frontmatter {
           date(formatString: "MMMM D, YYYY")
           title
+          slug
         }
         id
         excerpt
